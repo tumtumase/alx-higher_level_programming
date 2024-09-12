d1373ab272f4d034a25a575bb50a27179e17d13f
tumtumase <moecabolerato@gmail.com>
2024-09-12 15:08:44 -0700

    1 calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div  # Import only the required functions
+from calculator_1 import add, sub, mul, div
 
-a = 10  # Define variable a
-b = 5   # Define variable b
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-# Perform calculations and print the results
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
