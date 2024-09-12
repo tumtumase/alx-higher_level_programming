98d5c745f61de1fbb44ee2094db666a6f6f61461
tumtumase <moecabolerato@gmail.com>
2024-09-12 15:00:12 -0700

    import function simple

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add  # Import the add function from add_0
 
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+a = 1  # Assign 1 to a
+b = 2  # Assign 2 to b
+
+# Print the result in the format: a + b = result
+print("{} + {} = {}".format(a, b, add(a, b)))
