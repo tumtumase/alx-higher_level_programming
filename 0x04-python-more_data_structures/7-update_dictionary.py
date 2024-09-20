24a70047242383633cdb6f793b58adcadcc61167
tumtumase <moecabolerato@gmail.com>
2024-09-20 10:59:08 -0700

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value  # Add or update the key with the new value
+    return a_dictionary  # Return the updated dictionary
