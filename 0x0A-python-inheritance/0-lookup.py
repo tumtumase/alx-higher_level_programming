c820405bfce4de261f50585009ba6d8864bb93f8
tumtumase <moecabolerato@gmail.com>
2024-10-14 18:10:26 -0700

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module to return a list of available attributes and methods of an object
+"""
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    
+    Args:
+        obj: The object to inspect.
+        
+    Returns:
+        A list of attributes and methods.
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+New read me for python project I am doing at 3am
