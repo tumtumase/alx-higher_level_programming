9bacb562ba836d5a032d7524ab019eb2cc92272e
tumtumase <moecabolerato@gmail.com>
2024-10-14 18:24:34 -0700

    lookup 3

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module to return a list of available attributes and methods of an object
+Module to return a list of available attributes and methods of an object.
 """
 
 def lookup(obj):
